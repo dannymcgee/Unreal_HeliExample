c932bbd06d5af88cb58eb965c8b96154b5395d61
Danny McGee <dannymcgee@gmail.com>
2023-03-23 12:19:49 -0400

    Empty project init

---
+++ Source/HeliExample/HeliExample.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "HeliExample.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, HeliExample, "HeliExample" );
---
+++ Source/HeliExample/HeliExampleGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "HeliExampleGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class HELIEXAMPLE_API AHeliExampleGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
